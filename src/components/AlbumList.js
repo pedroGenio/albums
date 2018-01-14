165f83489462e092987e63919f5b4d0380f7a0fd
Pedro Eugenio <pedroeugenioed@gmail.com>
2018-01-14 13:16:17 +1300

    axios and setState

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
+  state = { albums: [] };
+
   componentWillMount() {
-    console.log('componentWillMount in AlbumList');
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+    .then(response => this.setState({ albums: response.data }));
   }
 
   render() {
+    console.log(this.state);
     return (
       <View>
         <Text>Album List!!!!</Text>
